bba249ee51e4f66641a8bdc96a0bd67eecddae7a
Kanat <kozhakhmet888@gmail.com>
2025-11-27 21:58:46 +0800

    Initial Commit

---
+++ MyFavoritesTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFavoritesTable
+//
+//  Created by user269741 on 11/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
